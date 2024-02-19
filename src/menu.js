f3eb5904e86f1bda2c30dc1c4774743078760287
SYAN <syanays.git@gmail.com>
2024-02-19 20:17:49 +0300

    add menu DOM

---
+++ src/menu.js
@@ -0,0 +1,58 @@
+export default function loadMenu() {
+    const content = document.getElementById('content')
+    const title = document.createElement('h2')
+    const container = document.createElement('div')
+
+    title.classList.add('promo-text')
+    container.classList.add('menu')
+
+    title.textContent = 'Menu'
+
+    // Menu items
+    let menuItems = []
+
+    const item1 = new Item('sweet-potato.jpg', 'Sweet Potato Fries', 'Golden sweet potato fries with parsely on top and a side of ketchup.', 2.99);
+    const item2 = new Item('chickpea-salad.jpg', "Autumn's Bounty", "Roasted chickpeas, pumpkins and sliced apples. Adorned with raisins and fresh basil.", 4.99);
+    const item3 = new Item('banana-bread.jpg', 'Banana Bread', 'Golden sweet potato fries with parsely on top and a side of ketchup.', 3.99);
+    const item4 = new Item('chocolate-donut.jpg', 'Chocolate Donut', 'Golden sweet potato fries with parsely on top and a side of ketchup.', 0.99);
+    const item5 = new Item('sweet-potato.jpg', 'Sweet Potato Fries', 'Golden sweet potato fries with parsely on top and a side of ketchup.', 2.99);
+    const item6 = new Item('sweet-potato.jpg', 'Sweet Potato Fries', 'Golden sweet potato fries with parsely on top and a side of ketchup.', 2.99);
+
+    menuItems.push(item1, item2, item3, item4, item5, item6)
+    menuItems.forEach(item => container.appendChild(displayItem(item)));
+
+
+    content.appendChild(title)
+    content.appendChild(container)
+}
+
+function Item(img, name, desc, price) {
+    this.img = img;
+    this.name = name;
+    this.desc = desc;
+    this.price = price;
+}
+
+function displayItem(item) {
+    const div = document.createElement('div')
+    const textDiv = document.createElement('div')
+    const itemImg = document.createElement('img')
+    const itemName = document.createElement('h3')
+    const itemDesc = document.createElement('p')
+    const itemPrice = document.createElement('p')
+
+    itemImg.setAttribute('src', item.img);
+
+    itemName.textContent = item.name;
+    itemDesc.textContent = item.desc;
+    itemPrice.textContent = `$ ${item.price}`;
+
+    div.appendChild(itemImg)
+    textDiv.appendChild(itemName)
+    textDiv.appendChild(itemDesc)
+    textDiv.appendChild(itemPrice)
+
+    div.appendChild(textDiv)
+    
+    return div;
+}
