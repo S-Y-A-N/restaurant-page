d627d2a5ff6429ef1dded586ee69e1327195e7be
SYAN <syanays.git@gmail.com>
2024-03-07 18:41:08 +0300

    add middle section of homepage (incomplete)

--- src/home.js
+++ src/home.js
@@ -1,5 +1,12 @@
+import * as image from './index.js';
+
 export default function loadHome() {
     const content = document.getElementById('content')
+    
+    const top = document.createElement('div')
+    top.classList.add('top')
+
+    // top section
     const text1 = document.createElement('p')
     const text2 = document.createElement('p')
     const text3 = document.createElement('p')
@@ -15,8 +22,33 @@ export default function loadHome() {
     text3.textContent = 'Beyond dreams!'
     button.textContent = 'Order Now'
 
-    content.appendChild(text1)
-    content.appendChild(text2)
-    content.appendChild(text3)
-    content.appendChild(button)
+    top.appendChild(text1)
+    top.appendChild(text2)
+    top.appendChild(text3)
+    top.appendChild(button)
+
+    content.appendChild(top)
+
+    // middle section
+    const menuBanner = document.createElement('div')
+    const lBtn = document.createElement('button')
+    const rBtn = document.createElement('button')
+
+    const img1 = document.createElement('img')
+    const img2 = document.createElement('img')
+    const img3 = document.createElement('img')
+    
+    img1.setAttribute('src', image.ChickpeaSalad);
+    img2.setAttribute('src', image.BananaBread);
+    img3.setAttribute('src', image.ChocoDonut);
+    
+    menuBanner.classList.add('banner')
+    
+    menuBanner.appendChild(lBtn)
+    menuBanner.appendChild(img1)
+    menuBanner.appendChild(img2)
+    menuBanner.appendChild(img3)
+    menuBanner.appendChild(rBtn)
+
+    content.appendChild(menuBanner)
 }
